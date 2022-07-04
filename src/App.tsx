462268a5c38e8f4a698f5198d686ff3cbdc14213
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2022-07-04 16:33:24 +0200

    Added basic site view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <>
+            <header className="header">
+                <section className="header__section">
+                    <h1 className="header__title"><strong>Dronowisko</strong> - świat okiem drona</h1>
+                    <button className="header__button">Dodaj miejscówkę</button>
+                </section>
+                <nav className="menu">
+                    <ul className="menu__list">
+                        <li className="menu__item"><a className="menu__link" href="/">Strona główna</a></li>
+                        <li className="menu__item"><a className="menu__link" href="/register">Rejestracja</a></li>
+                        <li className="menu__item"><a className="menu__link" href="/login">Logowanie</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="main-content">
+                <div className="search">
+                    <h2 className="search__title">Wyszukiwarka</h2>
+                    <label>Miejscowość
+                        <input type="text"/>
+                    </label>
+                    <label>Ulica
+                        <input type="text"/>
+                    </label>
+                    <button className="search__button">Wyszukaj</button>
+                    <section className="weather-info">
+                        <h2 className="weather-info__title">Warunki pogodowe</h2>
+                    </section>
+                </div>
+                <div className="map">
+                    Mapa
+                </div>
+            </div>
+            <footer className="footer"><p className="footer__copyrights">&copy; {new Date().getFullYear()} Arkadiusz
+                Mastalerczuk</p></footer>
+        </>
+    );
 };
