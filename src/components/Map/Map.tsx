d417ff4ef16c805a92341ebd8de596e8d274dfe3
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2022-07-08 08:45:56 +0200

    Added Leaflet map integration.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useContext} from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import './Map.css';
+import 'leaflet/dist/leaflet.css';
+import '../../utils/fix-map-icon';
+import {SecondHeader} from "../common/SecondHeader";
+import {SearchContext} from "../../contexts/search.context";
 
 export const Map = () => {
+    const {city} = useContext(SearchContext);
+
     return <>
         <div className="map">
-            Mapa...
+            <SecondHeader text={`Search for: ${city} `}/>
+            <MapContainer center={[50.259490, 19.022222]} zoom={16}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyritht'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[50.265759, 19.025570]}>
+                    <Popup>
+                        <h3>Spodek</h3>
+                        <p>Hala widowiskowo-sportowa</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     </>
 }
