543114a982acb039c06d146e8f8bb6bed05495e6
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2022-07-11 21:37:26 +0200

    Fixing CSS files.

--- src/components/HeaderSection/HeaderSection.tsx
+++ src/components/HeaderSection/HeaderSection.tsx
@@ -6,9 +6,7 @@ export const HeaderSection = () => {
     return (
         <section className="header__section">
             <h1 className="header__title"><strong>Dronowisko</strong> - świat okiem drona</h1>
-            <Btn text="Dodaj miejscówkę" style={{
-                marginRight: "200px",
-            }}/>
+            <Btn to="/add" text="Dodaj miejscówkę"/>
         </section>
     )
 }
--- src/components/common/Btn.tsx
+++ src/components/common/Btn.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Btn.css'
+import {Link} from "react-router-dom";
 
 interface Props {
     text: string;
+    to?: string;
     style?: {};
 }
 
 export const Btn = (props: Props) => (
-    <button className="btn" style={props.style}>{props.text}</button>
+    props.to
+        ? <Link className="btn" to={props.to} style={props.style}>{props.text}</Link>
+        : <button style={props.style}>{props.text}</button>
 )
